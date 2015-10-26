02506a0d708a21fd3b9d780fc068dff682610cfc
dragonist <erin314@naver.com>
2015-10-26 16:54:14 +0900

    git init

---
+++ apiserver/src/main/java/com/babydear/ApiserverApplication.java
@@ -0,0 +1,12 @@
+package com.babydear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiserverApplication.class, args);
+    }
+}
