3f9b1c46f4d69da1d73f2205388a39c933d7ca2d
030ii <lovelygeee2@gmail.com>
2015-11-16 05:57:33 +0900

    [landing] 랜딩페이지 html/css 완료

---
+++ web/ready/js/react-build/card.js
@@ -0,0 +1,13 @@
+// tutorial1.js
+var CommentBox = React.createClass({
+  displayName: "CommentBox",
+
+  render: function () {
+    return React.createElement(
+      "div",
+      { className: "commentBox" },
+      "Hello, world! I am a CommentBox."
+    );
+  }
+});
+React.render(React.createElement(CommentBox, null), document.getElementById('timeline_card'));
