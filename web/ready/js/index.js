e529088d086f0fbd645c8fb1d60f26028d563cf0
milooy <milooy@icloud.com>
2015-11-16 10:45:28 +0900

    [main] 일상탭, 기념탭

--- web/ready/js/index.js
+++ web/ready/js/index.js
@@ -3,5 +3,10 @@
 
     $('.button-collapse').sideNav();
 
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
