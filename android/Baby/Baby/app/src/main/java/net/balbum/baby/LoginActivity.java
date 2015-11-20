a50ef4a8dd5fa9c4eccdc8a82571df4815fc4964
Joo Hye Yeoun <hyeyeounj@naver.com>
2015-11-20 18:08:12 +0900

    [추가] 카드 이미지 activity: lib추가, CardWritingActivity-viewPager변경

---
+++ android/Baby/Baby/app/src/main/java/net/balbum/baby/LoginActivity.java
@@ -0,0 +1,207 @@
+package net.balbum.baby;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import net.balbum.baby.VO.LoginVo;
+import net.balbum.baby.lib.retrofit.ServiceGenerator;
+import net.balbum.baby.lib.retrofit.TaskService;
+
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+/**
+ * Created by hyes on 2015. 11. 15..
+ */
+public class LoginActivity extends FragmentActivity{
+    CallbackManager callbackManager;
+    TextView info;
+    Context context;
+    String tokenB;
+    SharedPreferences sharedPreferences;
+    String callValue;
+    LinearLayout linear;
+    Button login_button_origin;
+    EditText email, password;
+    String emailString, passwordString;
+    TaskService taskService;
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        taskService = ServiceGenerator.createService(TaskService.class);
+
+//        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+//        if(sharedPreferences.contains("tokenB")) {
+//            String t = sharedPreferences.getString("tokenB", "");
+//            Log.i("test", "hello~~"+ t);
+//            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//            startActivity(intent);
+//
+//        }
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        context = this;
+
+
+
+
+        linear = (LinearLayout)findViewById(R.id.layout);
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.login_activity);
+
+
+//        callValue = sharedPreferences.getString("token", "default value");
+//        Log.i("test", "callvalue" + callValue);
+//        if(callValue != null){
+//            goMainActivity();
+//        }
+
+        password = (EditText)findViewById(R.id.user_password);
+        email = (EditText)findViewById(R.id.user_email);
+
+        login_button_origin = (Button)findViewById(R.id.login_button_origin);
+
+        login_button_origin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                doLogin();
+                Toast.makeText(context, "Email: "+ email.getText().toString() + " password: "+ password.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
+//        login_button_origin.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                {
+//                    if (email.toString() == null) {
+//                        Snackbar.make(linear, "Fill in username.", Snackbar.LENGTH_SHORT).show();
+//
+//                    } else if (password.toString() == null) {
+//                        Snackbar.make(linear, "Fill in password.", Snackbar.LENGTH_SHORT).setAction("Clear", new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View v) {
+//
+//                            }
+//                        }).show();
+//                    } else{
+////                        Snackbar.make(linear, "You are logged in.", Snackbar.LENGTH_SHORT).show();
+//                    }
+//                }
+//            }
+//        });
+
+        LoginButton loginButton = (LoginButton)findViewById(R.id.login_button);
+        info = (TextView)findViewById(R.id.info);
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+
+
+
+
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                info.setText(
+                        "User ID: "
+                                + loginResult.getAccessToken().getUserId()
+                                + "\n" +
+                                "Auth Token: "
+                                + loginResult.getAccessToken().getToken()
+                        //Token을 sharedprefernce에 저장해두면 되겠군!
+                );
+
+//                token = loginResult.getAccessToken().getToken();
+             //   saveToken(context);
+
+                Handler handler = new Handler();
+                handler.postDelayed(runnable, 1000);
+
+            }
+
+            Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            };
+
+            @Override
+            public void onCancel() {
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+
+            }
+        });
+    }
+
+    private void doLogin() {
+        emailString = email.getText().toString();
+        passwordString = password.getText().toString();
+        LoginVo loginVo = new LoginVo(emailString, passwordString);
+        taskService.createLogin(loginVo, new Callback<LoginVo>() {
+            @Override
+            public void success(LoginVo task, Response response) {
+                Toast.makeText(context, "Login 성공~~~~", Toast.LENGTH_SHORT).show();
+
+                Log.i("test", "task: " + task + "body: " + response.getBody()+ " token: " + task.getToken());
+                saveTokenBalbum(context, task.getToken());
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                Toast.makeText(context, "fail", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void goMainActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
+    private void saveTokenBalbum(Context context, String token) {
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("tokenB", token);
+        Toast.makeText(context, "tokenB저" + token, Toast.LENGTH_SHORT).show();
+        editor.commit();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+}
---
+++ android/Baby/Baby/app/src/main/java/net/balbum/baby/adapter/BabyTagAdapter.java
@@ -0,0 +1,134 @@
+package net.balbum.baby.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import net.balbum.baby.R;
+import net.balbum.baby.Util.ConvertFileToBitmapUtil;
+import net.balbum.baby.VO.BabyTagVo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by hyes on 2015. 11. 11..
+ */
+public class BabyTagAdapter extends RecyclerView.Adapter<BabyTagAdapter.BabyViewHolder> {
+
+    private List<BabyTagVo> names;
+    private Context context;
+    private ArrayList<String> selectedList =new ArrayList<String>();
+
+
+
+
+    public BabyTagAdapter(List<BabyTagVo> names, Context context) {
+        this.names = names;
+        this.context = context;
+
+        if(names != null && names.size() == 1){
+            names.get(0).isSelected = true;
+        }
+    }
+
+    @Override
+    public BabyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.baby_names_row, parent, false);
+        BabyViewHolder pvh = new BabyViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(final BabyViewHolder holder, final int position) {
+        holder.photo.setImageBitmap(ConvertFileToBitmapUtil.convertBitmap(names.get(position).image));
+        holder.name.setText(names.get(position).name);
+
+        if (!names.get(position).isSelected) {
+            holder.photo.setAlpha(0.3f);
+        } else {
+            holder.photo.setAlpha(1.0f);
+        }
+
+        holder.photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                names.get(position).isSelected = !names.get(position).isSelected;
+                notifyDataSetChanged();
+
+            }
+        });
+    }
+
+    public ArrayList<String> getSelectedNames(){
+        for(int i =0; i< names.size(); i++){
+            if(names.get(i).isSelected){
+                selectedList.add(names.get(i).name);
+            }
+        }
+        return selectedList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return names.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+//    @Override
+//    public View getView(int position, View convertView, ViewGroup parent) {
+
+//        BabyViewHolder viewHolder;
+//
+//        if (convertView == null) {
+//            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.baby_names_row, parent, false);
+//
+//
+//            viewHolder = new BabyViewHolder();
+//            viewHolder.photo = (ImageView) convertView.findViewById(R.id.baby_iv);
+//            viewHolder.name = (CheckBox) convertView.findViewById(R.id.baby_cb);
+//
+//
+//            convertView.setTag(viewHolder);
+//        }
+//        // 캐시된 뷰가 있을 경우 저장된 뷰홀더를 사용한다
+//        else {
+//            viewHolder = (BabyViewHolder) convertView.getTag();
+//        }
+//
+//        viewHolder.photo.setImageBitmap(ConvertFileToBitmapUtil.convertBitmap(names.get(position).image));
+//        viewHolder.name.setText(names.get(position).name);
+//
+//
+//        return convertView;
+//    }
+
+
+    public ArrayList<String> getSelectedList() {
+        return selectedList;
+    }
+
+    class BabyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView photo;
+        TextView name;
+
+        public BabyViewHolder(View itemView) {
+            super(itemView);
+            photo = (ImageView)itemView.findViewById(R.id.baby_iv);
+            name = (TextView)itemView.findViewById(R.id.baby_tv);
+        }
+    }
+
+
+}
+
