54859eb89774e6b8fd406ff6f41447f2d1fea4a0
milooy <milooy@icloud.com>
2015-11-21 17:45:11 +0900

    [main] div들을 HTML5요소들로 고치기

--- web/ready/js/index.js
+++ web/ready/js/index.js
@@ -8,5 +8,20 @@
         selectYears: 15 // Creates a dropdown of 15 years to control year
     });
 
+    // function readURL(input) {
+    //     if (input.files && input.files[0]) {
+    //         var reader = new FileReader();
+
+    //         reader.onload = function (e) {
+    //             $('#blah').attr('src', e.target.result);
+    //         }
+    //         reader.readAsDataURL(input.files[0]);
+    //     }
+    // }
+
+    // $("#imgInp").change(function(){
+    //     readURL(this);
+    // });
+
   }); // end of document ready
 })(jQuery); // end of jQuery name space
