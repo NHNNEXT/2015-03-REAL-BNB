cb819498382a46ae4e82ee09d70510059042f581
milooy <milooy@icloud.com>
2015-11-22 00:55:44 +0900

    [main] index.js를 OOP로 고치기

--- web/ready/js/index.js
+++ web/ready/js/index.js
@@ -1,15 +1,19 @@
-// (function($){
-  $(function(){
+var Start = {
+    init: function() {
+        $('.button-collapse').sideNav();
+        $('.datepicker').pickadate({
+            selectMonths: true, // Creates a dropdown to control month
+            selectYears: 15 // Creates a dropdown of 15 years to control year
+        });
+    }
 
-    $('.button-collapse').sideNav();
+}
 
-    $('.datepicker').pickadate({
-        selectMonths: true, // Creates a dropdown to control month
-        selectYears: 15 // Creates a dropdown of 15 years to control year
-    });
-
-    // 포토 업로드
-    function readURL(input) {
+var Upload = {
+    init: function() {
+        this.uploadPhoto();
+    },
+    readURL: function(input) {
         if (input.files && input.files[0]) {
             var reader = new FileReader();
             reader.onload = function (e) {
@@ -17,18 +21,23 @@
             }
             reader.readAsDataURL(input.files[0]);
         }
+    },
+    uploadPhoto: function() {
+        // 업로드버튼이나 올려진 사진을 누르면 hidden된 form을 누른다.
+        $('.upload-photo-box, #uploaded-photo').click(function(event) {
+            $("#upload-photo").click();
+        });
+
+        // form이 변경되면 fileread를 한다.
+        $("#upload-photo").change(function(){
+            Upload.readURL(this);
+            $('.upload-photo-box').css('display', 'none');
+        });
     }
-    // 업로드버튼이나 올려진 사진을 누르면 hidden된 form을 누른다.
-    $('.upload-photo-box, #uploaded-photo').click(function(event) {
-         $("#upload-photo").click();
-    });
+}
 
-    // form이 변경되면 fileread를 한다.
-    $("#upload-photo").change(function(){
-        readURL(this);
-        $('.upload-photo-box').css('display', 'none');
-    });
+$(function(){
+    Start.init();
+    Upload.init();
+});
 
-
-  }); // end of document ready
-// })(jQuery); // end of jQuery name space
