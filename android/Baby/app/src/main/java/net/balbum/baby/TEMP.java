4eef02a4bd61bee3857cbbc692fb45e69a13ed70
Joo Hye Yeoun <hyeyeounj@naver.com>
2015-12-01 14:51:22 +0900

    [추가] URL로 이미지뷰에 이미지출력 테스트 확인 완료

--- android/Baby/app/src/main/java/net/balbum/baby/TEMP.java
+++ android/Baby/app/src/main/java/net/balbum/baby/TEMP.java
@@ -2,13 +2,13 @@ package net.balbum.baby;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -25,68 +25,51 @@ public class TEMP extends AppCompatActivity {
 InputStream is;
     Bitmap bit;
     ImageView iv;
+    TextView tv;
     HttpURLConnection connection;
+    Bitmap bmp = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.temp);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
+        if (android.os.Build.VERSION.SDK_INT > 9) {
+            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+            StrictMode.setThreadPolicy(policy);
+        }
 
         iv = (ImageView)findViewById(R.id.temp_img);
-
-
-        Log.d("test", "setImage1");
-//        iv.setImageBitmap(a);
-
+        tv = (TextView)findViewById(R.id.temp_tv);
 
         Handler handler = new Handler();
+
+        Thread thread = new Thread()
+
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
                 URL url = null;
                 try {
-
-                    String a = "http://192.168.1.146:8080/img/asdf.jpeg";
-                    url = new URL(a);
-                    try {
-                        connection = (HttpURLConnection) url.openConnection();
-                        connection.setDoInput(true);
-                        connection.connect();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-
-                    try {
-                        is= connection.getInputStream();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-//                    url = new URL(a);
+                    url = new URL("https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQV80K-5-fjT_RKsYIKFQjpVKhmQRH5k-xkq5yLKe9JslT0zasP");
+                    tv.setText(url.toString());
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
                 }
-                Bitmap bmp = null;
+
                 try {
                     bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    Log.d("test", "bmp: "+ bmp.getConfig());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
                 iv.setImageBitmap(bmp);
-                iv.setBackgroundColor(Color.parseColor("eeff85"));
 
-                iv.setImageBitmap(bit);
-                Log.d("test", "setImage2");
-
+                Log.d("test", "setImage1");
             }
-        },3000);
-
+        }, 1000);
 
     }
-
 
 
 }
