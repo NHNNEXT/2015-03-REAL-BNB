0a7606f22ad450743412019ee1101cd86c71706e
Joo Hye Yeoun <hyeyeounj@naver.com>
2015-12-01 15:04:42 +0900

    [수정] network 작업 thread 분리 테스트 완료

--- android/Baby/app/src/main/java/net/balbum/baby/TEMP.java
+++ android/Baby/app/src/main/java/net/balbum/baby/TEMP.java
@@ -4,7 +4,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.os.Handler;
-import android.os.StrictMode;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ImageView;
@@ -34,24 +34,23 @@ InputStream is;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.temp);
 
-        if (android.os.Build.VERSION.SDK_INT > 9) {
-            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-            StrictMode.setThreadPolicy(policy);
-        }
+//        if (android.os.Build.VERSION.SDK_INT > 9) {
+//            StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+//            StrictMode.setThreadPolicy(policy);
+//        }
 
-        iv = (ImageView)findViewById(R.id.temp_img);
-        tv = (TextView)findViewById(R.id.temp_tv);
 
-        Handler handler = new Handler();
+        final String a = "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQV80K-5-fjT_RKsYIKFQjpVKhmQRH5k-xkq5yLKe9JslT0zasP";
+        final String c = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYgdGMhHf6TaMiIwvslhKy-FfL77RLopOlYEAXOhyIwtBQbyZT";
+        iv = (ImageView) findViewById(R.id.temp_img);
+        tv = (TextView) findViewById(R.id.temp_tv);
 
-        Thread thread = new Thread()
-
-        handler.postDelayed(new Runnable() {
+        new Thread(new Runnable() {
             @Override
             public void run() {
                 URL url = null;
                 try {
-                    url = new URL("https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQV80K-5-fjT_RKsYIKFQjpVKhmQRH5k-xkq5yLKe9JslT0zasP");
+                    url = new URL(c);
                     tv.setText(url.toString());
                 } catch (MalformedURLException e) {
                     e.printStackTrace();
@@ -59,17 +58,32 @@ InputStream is;
 
                 try {
                     bmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-                    Log.d("test", "bmp: "+ bmp.getConfig());
+                    Log.d("test", "bmp: " + bmp.getConfig());
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                iv.setImageBitmap(bmp);
+
+                handler.sendEmptyMessage(0);
 
                 Log.d("test", "setImage1");
             }
-        }, 1000);
+        }).start();
+
 
     }
+
+    Handler handler = new Handler(new Handler.Callback(){
+
+        @Override
+        public boolean handleMessage(Message msg) {
+            if(msg != null){
+                iv.setImageBitmap(bmp);
+
+            }
+            return false;
+        }
+    });
+
 
 
 }
