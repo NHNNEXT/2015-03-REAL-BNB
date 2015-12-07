ba851edb8cf9202f34584662d606e20f9cb03ade
Joo Hye Yeoun <hyeyeounj@naver.com>
2015-12-07 13:43:53 +0900

    [가입] email sign_btn에서 main activity intent 연결

--- android/Baby/app/src/main/java/net/balbum/baby/SignEmailActivity.java
+++ android/Baby/app/src/main/java/net/balbum/baby/SignEmailActivity.java
@@ -1,16 +1,27 @@
 package net.balbum.baby;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 
 /**
  * Created by hyes on 2015. 12. 3..
  */
-public class SignEmailActivity extends AppCompatActivity{
+public class SignEmailActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_email_fragment);
+
+        findViewById(R.id.sign_btn).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(SignEmailActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
