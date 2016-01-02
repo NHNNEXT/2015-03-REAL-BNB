885e4d706f009a705733d055ed5ff18c1044b983
dragonist <erin314@naver.com>
2016-01-02 20:38:26 +0900

    [서버] joda time 에러 의심됨, 원인은 모르나 해결은 했음 external jar import 방법으로!

--- apiserver/src/test/java/com/babydear/domain/BabyTest.java
+++ apiserver/src/test/java/com/babydear/domain/BabyTest.java
@@ -3,32 +3,11 @@ package com.babydear.domain;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-import org.joda.time.DateTime;
-import org.joda.time.Days;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.babydear.ApiserverApplication;
-import com.babydear.dto.UserDTO;
 import com.babydear.model.Baby;
-import com.babydear.model.Card;
-import com.babydear.model.Family;
-import com.babydear.model.User;
-import com.babydear.repository.BabyRepository;
-import com.babydear.repository.CardRepository;
-import com.babydear.repository.FamilyRepository;
-import com.babydear.repository.UserRepository;
 import com.babydear.util.DateFormatter;
 
 public class BabyTest {
