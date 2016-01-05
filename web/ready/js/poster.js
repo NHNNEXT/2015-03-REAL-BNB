a30ebf4f492bb7b3696bafb812eeeb3d3fa3744e
030ii <lovelygeee2@gmail.com>
2016-01-06 07:15:55 +0900

    [후가공] 템플릿 선택하기 css 완료

--- web/ready/js/poster.js
+++ web/ready/js/poster.js
@@ -10,14 +10,15 @@ $(function() {
 	// });
 
 	// select template
-	$( "section" ).delegate( ".poster-template", "click", function(e) {   // 그래서 event delegate가 필요함. 그런데 지금보니 index.js에 posterController 실행될때 js로 써도됨.
-  		console.log(this);
-  		// if ($(this) == $(".selected-poster"){
-  			
-  		// }
-  		// $( this ).toggleClass( "selected-poster" ); // 카드를 선택할 때마다 'chosen'이라는 토글 클래스 추가/제거 
+	// $( "section" ).delegate( ".ff", "click", function(e) {   // 그래서 event delegate가 필요함. 그런데 지금보니 index.js에 posterController 실행될때 js로 써도됨.
+ //  		console.log(this);
 
-	});
+ //  		// if ($(this) == $(".selected-poster"){
+
+ //  		// }
+ //  		// $( this ).toggleClass( "selected-poster" ); // 카드를 선택할 때마다 'chosen'이라는 토글 클래스 추가/제거 
+
+	// });
 
 	// select cards
 	$( "section" ).delegate( ".select-for-poster", "click", function(e) {   // 그래서 event delegate가 필요함. 그런데 지금보니 index.js에 posterController 실행될때 js로 써도됨.
