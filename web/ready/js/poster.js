9dc664ab9df2e27e019ccab14d3a0eb790dbdf58
030ii <lovelygeee2@gmail.com>
2016-01-06 05:47:38 +0900

    [후가공] 포스터 뀨

--- web/ready/js/poster.js
+++ web/ready/js/poster.js
@@ -9,7 +9,17 @@ $(function() {
 	//     // this.data("cid")~~~
 	// });
 
+	// select template
+	$( "section" ).delegate( ".poster-template", "click", function(e) {   // 그래서 event delegate가 필요함. 그런데 지금보니 index.js에 posterController 실행될때 js로 써도됨.
+  		console.log(this);
+  		// if ($(this) == $(".selected-poster"){
+  			
+  		// }
+  		// $( this ).toggleClass( "selected-poster" ); // 카드를 선택할 때마다 'chosen'이라는 토글 클래스 추가/제거 
 
+	});
+
+	// select cards
 	$( "section" ).delegate( ".select-for-poster", "click", function(e) {   // 그래서 event delegate가 필요함. 그런데 지금보니 index.js에 posterController 실행될때 js로 써도됨.
   		$( this ).toggleClass( "chosen" ); // 카드를 선택할 때마다 'chosen'이라는 토글 클래스 추가/제거 
 
